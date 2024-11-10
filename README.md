c8f5866b87dc026af7d9580f41cbff993598d514
johnlohjy <johnlohjy@gmail.com>
2024-11-11 02:56:55 +0800

    Checkpoint 1: Uploaded and stream dataset from HF

--- README.md
+++ README.md
@@ -1,31 +1,51 @@
 # File Directory
 
-**Focus on**
+**Key Files**
 
-```prepare_dataset.ipynb```: Prepare the dataset to be uploaded to HF
+```prepare_dataset.ipynb```: Prepare the dataset (whole of part 3 same close mic) to be uploaded to HF
 
-```imda_nsc_testing.ipynb```: Dataset loading script to be uploaded to HF
+```prepare_dataset_small.ipynb```: Prepare a portion of the dataset (in terms of hours) to be upload to HF
 
-```use_dataset.ipynb```: Short example on how to stream from the dataset
+```imda_nsc_p3_etc.ipynb```: Dataset loading script to be uploaded to its respective HF dataset
+
+```use_dataset_etc.ipynb```: Short example on how to stream from its respective HF dataset
 
 <br/>
 <br/>
 <br/>
 
-# Steps
+# Steps to use dataset
+
+### Step 1: Refer to ```use_dataset_small_train.ipynb``` to see an example on how to stream 50 hours of training data
+
+### Step 2: Refer to ```use_dataset_small_test.ipynb``` to see an example on how to stream 6 hours of test data
+
+<br/>
+<br/>
+<br/>
+
+# Dataset Links
+
+- https://huggingface.co/datasets/johnlohjy/imda_nsc_p3_same_closemic_small_train
+    - 50 hours of training data
+- https://huggingface.co/datasets/johnlohjy/imda_nsc_p3_same_closemic_small_test
+    - 6 hours of test data
+- https://huggingface.co/datasets/johnlohjy/imda_nsc_p3_same_closemic
+    - All data
+
+<br/>
+<br/>
+<br/>
+
+# Steps to create dataset <u>from scratch</u>
 
 ### Step 1: Create a new HF dataset repository
 
 ![alt text](images/HF_create_dataset_repo.png)
 
-### Step 2: Follow the instructions and run ```prepare_dataset.ipynb```
-- Just take note of the parts with **USER INPUT REQUIRED** in Step 1 only
-    - Change the relative paths and naming conventions if you want (but not needed)
-    - Add in the ```.wav``` and ```.TextGrid``` files from IMDA NSC into ```org_waves``` and ```org_transcript``` respectively after the directory has been initialised
-        - Use <u>Audio Same CloseMic</u> folder for the ```.wav``` files first (from Part 3 in the DropBox)
-        - Use <u>Scripts Same</u> folder for the ```.TextGrid``` files first (from Part 3 in the DropBox)
+### Step 2: Follow the instructions in ```prepare_dataset.ipynb``` and run ```prepare_dataset.ipynb```
 
-### Step 3: Upload the ```data``` folder containing the compressed files to the created dataset repository
+### Step 3: Upload the ```data``` folder created from Step 2 containing the compressed files to the created dataset repository
 
 ![alt text](images/local_folder_structure_1.png)
 
@@ -39,10 +59,14 @@
 
 ### Step 4: Modify and upload the loading script as needed. Note that it has to have the same name as the created HF dataset repo
 
-Example if your HF dataset repo is called ```imda_nsc_testing```, your loading script has to be called ```imda_nsc_prototype.py```
+Example: If your HF dataset repo is called ```imda_nsc_p3_testing```, your loading script has to be called ```imda_nsc_p3_testing.py```
 
 Modify the paths in the loading script as needed
 
 ![alt text](images/HF_upload_loading_script.png)
 
-### Step 5: Refer to ```use_dataset.ipynb``` on how to stream the dataset and its splits
+### Step 5: Refer to ```use_dataset_etc.ipynb``` on how to stream the dataset and its splits
+
+### Step 6: Repeat Step 2 to Step 5, but use ```prepare_dataset_small.ipynb``` instead of ```prepare_dataset.ipynb```. 
+- This is to use less hours of data as the original dataset has 500+ hours of training data
+- By default, ```prepare_dataset_small.ipynb``` cuts it to 50 hours
---
+++ imda_nsc_p3_same_closemic_small.py
@@ -0,0 +1,127 @@
+# Copyright 2020 The HuggingFace Datasets Authors and the current dataset script contributor.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# TODO: Address all TODOs and remove all explanatory comments
+"""TODO: Add a description here."""
+
+import csv
+import json
+import os
+
+import datasets
+
+_CITATION = """\
+@InProceedings{huggingface:dataset,
+title = {A great new dataset},
+author={huggingface, Inc.
+},
+year={2020}
+}
+"""
+
+_DESCRIPTION = """\
+This new dataset is designed to solve this great NLP task and is crafted with a lot of care.
+"""
+
+_HOMEPAGE = ""
+
+_LICENSE = ""
+
+
+# TODO: Add link to the official dataset URLs here
+_DATA_URL = "data/imda_nsc_p3_small.tar.gz"
+
+_PROMPTS_URLS = {
+    "train": "data/prompts-train-small.txt.gz",
+    "test": "data/prompts-test-small.txt.gz"
+}
+
+# TODO: Name of the dataset usually matches the script name with CamelCase instead of snake_case
+class SinglishDataset(datasets.GeneratorBasedBuilder):
+    """TODO: Short description of my dataset."""
+
+    VERSION = datasets.Version("1.1.0")
+
+    def _info(self):
+        return datasets.DatasetInfo(
+            description=_DESCRIPTION,
+            features=datasets.Features(
+                {
+                    "path": datasets.Value("string"),
+                    "audio": datasets.Audio(sampling_rate=16_000),
+                    "sentence": datasets.Value("string"),
+                }
+            ),
+            homepage=_HOMEPAGE,
+            license=_LICENSE,
+            citation=_CITATION,
+        )
+
+    def _split_generators(self, dl_manager):
+        # TODO: This method is tasked with downloading/extracting the data and defining the splits depending on the configuration
+        # If several configurations are possible (listed in BUILDER_CONFIGS), the configuration selected by the user is in self.config.name
+
+        prompts_paths = dl_manager.download_and_extract(_PROMPTS_URLS) 
+        archive = dl_manager.download(_DATA_URL) 
+        train_dir = "../train"
+        test_dir = "../test"
+        return [
+            datasets.SplitGenerator(
+                name=datasets.Split.TRAIN, 
+                gen_kwargs={ 
+                    "prompts_path": prompts_paths["train"], 
+                    "path_to_clips": train_dir + "/waves", 
+                    "audio_files": dl_manager.iter_archive(archive), 
+                },
+            ),
+            datasets.SplitGenerator(
+                name=datasets.Split.TEST, 
+                gen_kwargs={ 
+                    "prompts_path": prompts_paths["test"], 
+                    "path_to_clips": test_dir + "/waves", 
+                    "audio_files": dl_manager.iter_archive(archive), 
+                },
+            )
+        ]
+    
+    def _generate_examples(self, prompts_path, path_to_clips, audio_files):
+        # TODO: This method handles input defined in _split_generators to yield (key, example) tuples from the dataset.
+        # The `key` is for legacy reasons (tfds) and is not important in itself, but must be unique for each example.
+        
+        examples = {}
+        with open(prompts_path, encoding="utf-8") as f:
+            for row in f:
+                data = row.strip().split(" ", 1) 
+                audio_path = "/".join([path_to_clips, data[0] + ".wav"]) 
+                print("Audio Path")
+                print(audio_path)
+                examples[audio_path] = {
+                    "path": audio_path,
+                    "sentence": data[1],
+                }
+
+        inside_clips_dir = False
+        id_ = 0
+        for path, f in audio_files:
+            print("Path")
+            print(path)
+            print("Path to clips")
+            print(path_to_clips)
+            if path.startswith(path_to_clips):
+                inside_clips_dir = True
+                if path in examples:
+                    audio = {"path": path, "bytes": f.read()}
+                    yield id_, {**examples[path], "audio": audio}
+                    id_ += 1
+            elif inside_clips_dir:
+                break
